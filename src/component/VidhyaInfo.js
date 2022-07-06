d47c0bf89be99e07607a24ff57c8305fb94f65f1
HTML <vidhyasrinivasan07@gmail.com>
2022-07-05 20:18:02 -0500

    VidhyaInfo page

---
+++ src/component/VidhyaInfo.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function VidhyaInfo(props){
+    return (
+        <div style={{backgroundColor:'Gray', width:500,height:800 }}>
+            <h1 style={{color:'blue'}}>VidhyaraniPandiarajan</h1><br></br>
+            
+            <h1 style={{color:'blue'}}>Technical Skills:</h1>
+        
+              <h2>  HTML, CSS</h2>
+                <h2>JavaScript</h2>
+                <h2>Java</h2>
+                <h2>NS2</h2><br></br>
+        
+        <h1 style={{color:'blue'}}>Degree:</h1>
+        <ul><li>Bachelor of Engineering- Computer Science</li>
+        <li>Masters in Engineering - Communication and Networking</li>
+        </ul><br></br>
+        <h1 style={{color:'blue'}}> Personal Interests:</h1>
+        <ul>
+            <li>Cooking</li>
+            <li>Travelling</li>
+        </ul>
+        
+        
+        </div>
+    );
+}
+
+export default VidhyaInfo;
+
